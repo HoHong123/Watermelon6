1354e6fad6ca2d51ff5d636ba236842113e9690f
PKH <epzmfhs@gmail.com>
2025-08-27 19:10:41 +0900

    [Refact] ♻️ 27.08.25 Clean game manager

--- Assets/01_Scripts/Game/GameManager.cs
+++ Assets/01_Scripts/Game/GameManager.cs
@@ -1,20 +1,37 @@
+using System.Collections.Generic;
 using UnityEngine;
+using Util.Pooling;
 
 namespace Melon.Game {
     public class GameManager : SingletonBehaviour<GameManager> {
-        [Tooltip("레벨 낮은 순서대로 등록 (0,1,2,...)")]
-        public GameObject[] fruitPrefabs;
-
-        [Tooltip("합체 시 살짝 위로 튕기는 힘")]
+        public FruitCtrl[] fruitPrefabs;
         public float mergeBounceForce = 2f;
+        Dictionary<FruitType, ComponentPool<FruitCtrl>> fruitPools;
 
-        
-        public bool CanUpgrade(int level) {
-            return level + 1 < fruitPrefabs.Length;
+
+        private void Start() {
+            foreach (var fruit in fruitPrefabs) {
+                var pool = new ComponentPool<FruitCtrl>(
+                    fruit,
+                    0,
+                    new GameObject($"{fruit.Type.ToString()}_FruitPool").transform,
+                    (obj) => obj.OnCreate(),
+                    (obj) => obj.OnGet(),
+                    (obj) => obj.OnReturn(),
+                    (obj) => obj.OnDispose()
+                    );
+
+                fruitPools.Add(fruit.Type, pool);
+            }
         }
 
-        public void MergeAt(Vector2 position, int level) {
-            int nextLevel = Mathf.Min(level + 1, fruitPrefabs.Length - 1);
+        public bool CanUpgrade(FruitType level) {
+            return true;
+            //return level + 1 < fruitPrefabs.Length;
+        }
+
+        public void MergeAt(Vector2 position, FruitType level) {
+            int nextLevel = Mathf.Min((int)level + 1, fruitPrefabs.Length - 1);
             var go = Instantiate(fruitPrefabs[nextLevel], position, Quaternion.identity);
             var rb = go.GetComponent<Rigidbody2D>();
             if (rb != null) {
