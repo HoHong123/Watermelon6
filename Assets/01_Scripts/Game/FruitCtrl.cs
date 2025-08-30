1f774801ff791590955e930c110ef4fefff55df7
JasonCatze <epzmfhs@gmail.com>
2025-08-31 06:30:33 +0900

    [Feat] 🆕 31.08.25 Update fruit controller.
    1. Clean code.
    2. Apply pooling system.
    3. Add physic activation features.
    4. Add extra properties.

--- Assets/01_Scripts/Game/FruitCtrl.cs
+++ Assets/01_Scripts/Game/FruitCtrl.cs
@@ -1,6 +1,7 @@
-using Sirenix.OdinInspector;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace Melon.Game {
     [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
@@ -8,19 +9,26 @@ namespace Melon.Game {
         [Title("Data")]
         [SerializeField]
         FruitType type;
-        public FruitType Type => type;
-        public int Level => (int)type;
-        public int Compare(FruitType x, FruitType y) => x.CompareTo(y);
 
         [Title("Physic")]
         [SerializeField]
-        BoxCollider2D box2D;
+        Rigidbody2D r2d;
+        [SerializeField]
+        CircleCollider2D circle2D;
 
-        public float mergeCooldown = 0.15f;
-
+        float mergeCooldown = 0.15f;
         float lastMergeTime = -999f;
 
+        public FruitType Type => type;
+        public int Level => (int)type;
+        public int Compare(FruitType x, FruitType y) => x.CompareTo(y);
+        public bool DoneSpawn { get; private set; }
+        public bool IsMergeCooldownReady => (Time.time - lastMergeTime < mergeCooldown);
 
+        public event Action OnDoneSpawn;
+
+
+        #region Pool
         public void OnCreate() {
             gameObject.SetActive(false);
         }
@@ -30,50 +38,59 @@ namespace Melon.Game {
         }
 
         public void OnGet() {
-            box2D.enabled = false;
+            PhysicDeactive();
             gameObject.SetActive(true);
         }
 
         public void OnReturn() {
-            box2D.enabled = false;
+            PhysicDeactive();
             gameObject.SetActive(false);
+        }
+        #endregion
+
+        #region Activation
+        public void PhysicActive() {
+            r2d.simulated = true;
+            circle2D.enabled = true;
+        }
+
+        public void PhysicDeactive() {
+            r2d.simulated = false;
+            circle2D.enabled = false;
+        }
+        #endregion
+
+        public void Bounce(float bounceForce) {
+            r2d.AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
+            r2d.AddTorque(UnityEngine.Random.Range(-0.3f, 0.3f), ForceMode2D.Impulse);
         }
 
 
         private void OnCollisionEnter2D(Collision2D collision) {
+            DoneSpawn = true;
+
             if (!collision.collider.CompareTag("Fruit")) return;
 
             FruitCtrl other = collision.collider.GetComponent<FruitCtrl>();
-            if (other == null) return;
-            if (Compare(type, other.type) < 1) return;
 
-            // 쿨다운(양쪽 모두 최근에 합체했다면 스킵)
-            if (Time.time - lastMergeTime < mergeCooldown)
-                return;
-            if (Time.time - other.lastMergeTime < mergeCooldown)
-                return;
-
-            // 한 쪽만 진행(InstanceID가 큰 쪽이 owner)
-            if (GetInstanceID() < other.GetInstanceID())
-                return;
-
-            // 최종 레벨이면 더 이상 업그레이드 불가 (그냥 튕기기만)
-            if (!GameManager.Instance.CanUpgrade(type))
-                return;
-
-            // 합체 위치 = 두 과일의 중간점
+            if (other == null) return; // Check object
+            if (Compare(type, other.type) != 0) return; // Check level
+            if (IsMergeCooldownReady || other.IsMergeCooldownReady) return; // Check cooldown
+            if (GetInstanceID() < other.GetInstanceID()) return; // Activate one of fruit controller
+            if (!GameManager.Instance.CanUpgrade(type)) return; // Check level
+            
+            // Find mid point
             Vector2 mid = (Vector2)transform.position * 0.5f + (Vector2)other.transform.position * 0.5f;
-
-            // 합체 실행
-            GameManager.Instance.MergeAt(mid, type);
-
-            // 쿨다운 마킹
+            // Record cooldown
             lastMergeTime = Time.time;
             other.lastMergeTime = Time.time;
 
-            // 기존 둘 삭제
-            Destroy(other.gameObject);
-            Destroy(gameObject);
+            // Merge
+            GameManager.Instance.MergeAt(mid, type);
+
+            // Return to pool
+            GameManager.Instance.Return(other);
+            GameManager.Instance.Return(this);
         }
     }
 }
