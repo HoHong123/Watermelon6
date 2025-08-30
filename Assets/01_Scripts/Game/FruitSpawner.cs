d36828c9c1ceb5b982b6733ec9901b203b8675f5
JasonCatze <epzmfhs@gmail.com>
2025-08-31 06:31:08 +0900

    [Feat] 🆕 31.08.25 Update spawner.

--- Assets/01_Scripts/Game/FruitSpawner.cs
+++ Assets/01_Scripts/Game/FruitSpawner.cs
@@ -1,23 +1,29 @@
+using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Sirenix.OdinInspector;
-using Unity.VisualScripting;
 
 namespace Melon.Game {
     public class FruitSpawner : MonoBehaviour {
-        [Title("Spawn")]
+        [Title("UI")]
+        [SerializeField]
+        List<GameObject> spawnIcons;
+
+        [Title("Boundary")]
         [SerializeField]
         Transform spawnPoint;
         [SerializeField]
         BoxCollider2D spawnBoundary;
-        [SerializeField, PropertyRange(1f, 2f)]
-        float spawnDelay = 1.6f;
         [SerializeField]
         bool dropOnRelease = true;     // 손 뗄 때 드랍(권장)
 
-        [Title("Camera")]
+        [Title("Delay")]
+        [SerializeField, PropertyRange(0, 2f)]
+        float spawnDelay = 1.6f;
         [SerializeField]
-        Camera cam = Camera.main;
+        bool spawnStop = false;
 
         [Title("Movement")]
         [SerializeField]
@@ -25,42 +31,55 @@ namespace Melon.Game {
         [SerializeField]
         float moveLerp = 20f;
 
+        Camera cam;
+        FruitCtrl currentFruit;
+
+
+        private void Start() {
+            cam = Camera.main;
+            GameManager.Instance.OnGameOver += _OnGameOver;
+        }
 
         private void Update() {
             // Ignore UI interaction
-            if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject())
-                return;
-
+            if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject()) return;
             _HandlePointer();
         }
 
 
         private void _HandlePointer() {
-#if UNITY_ANDROID || UNITY_IOS
-            // Touch
-            if (Input.touchCount > 0) {
-                var t = Input.GetTouch(0);
-                _FollowPointerX(t.position);
-
-                if (dropOnRelease && t.phase == TouchPhase.Ended)
-                    SpawnFruit();
-                else if (!dropOnRelease && t.phase == TouchPhase.Began)
-                    SpawnFruit();
-            }
-#else
+#if UNITY_EDITOR
             // Mouse
             if (Input.GetMouseButton(0)) {
                 _FollowPointerX(Input.mousePosition);
             }
-            if (dropOnRelease) {
-                if (Input.GetMouseButtonUp(0))
-                    _SpawnFruit();
+
+            if (spawnStop) return;
+
+            if (Input.GetMouseButtonDown(0) && currentFruit == null) {
+                _SpawnFruit();
             }
-            else {
-                if (Input.GetMouseButtonDown(0))
-                    _SpawnFruit();
+
+            if (Input.GetMouseButtonUp(0)) {
+                _ActiveFruit();
+            }
+#elif UNITY_ANDROID || UNITY_IOS
+            // Touch
+            if (Input.touchCount > 0) {
+                var t = Input.GetTouch(0);
+                _FollowPointerX(t.position);
+                
+                if (spawnStop) return;
+
+                if (t.phase == TouchPhase.Ended)
+                    _ActiveFruit();
             }
 #endif
+        }
+
+
+        private void _OnGameOver() {
+            enabled = false;
         }
 
         private void _FollowPointerX(Vector3 screenPos) {
@@ -71,6 +90,7 @@ namespace Melon.Game {
             Vector3 spawn = spawnPoint.position;
             spawn.x = Mathf.Lerp(spawn.x, targetX, Time.deltaTime * moveLerp);
             spawnPoint.position = spawn;
+            currentFruit.transform.position = spawn;
         }
 
         private float _ClampX(float x) {
@@ -92,8 +112,22 @@ namespace Melon.Game {
         }
 
         private void _SpawnFruit() {
-            var prefab = GameManager.Instance.fruitPrefabs[0];
-            Instantiate(prefab, spawnPoint.position, Quaternion.identity);
+            //currentFruit = GameManager.Instance.GetRandomBasedOnScore();
+            currentFruit = GameManager.Instance.Get(FruitType.Pineapple);
+            currentFruit.transform.position = spawnPoint.position;
+            currentFruit.transform.eulerAngles = Vector3.zero;
+        }
+
+        private void _ActiveFruit() {
+            _SpawnDelay();
+            currentFruit.PhysicActive();
+            currentFruit = null;
+        }
+
+        private async void _SpawnDelay() {
+            spawnStop = true;
+            await UniTask.Delay(TimeSpan.FromSeconds(spawnDelay));
+            spawnStop = false;
         }
     }
 }
