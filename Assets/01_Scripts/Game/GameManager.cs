282f7cf206080f268102e62940f0bafc265fffb1
JasonCatze <epzmfhs@gmail.com>
2025-08-31 06:31:24 +0900

    [Feat] 🆕 31.08.25 Update Game Manager.

--- Assets/01_Scripts/Game/GameManager.cs
+++ Assets/01_Scripts/Game/GameManager.cs
@@ -1,12 +1,41 @@
+using System;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using Random = UnityEngine.Random;
+using Sirenix.OdinInspector;
 using Util.Pooling;
 
 namespace Melon.Game {
     public class GameManager : SingletonBehaviour<GameManager> {
-        public FruitCtrl[] fruitPrefabs;
-        public float mergeBounceForce = 2f;
-        Dictionary<FruitType, ComponentPool<FruitCtrl>> fruitPools;
+        // ¸â¹ö ¿µ¿ª ¾îµò°¡¿¡ Ãß°¡
+        [Title("State")]
+        [ShowInInspector, ReadOnly]
+        public bool IsGameOver { get; private set; }
+
+        [Title("Prefabs")]
+        [SerializeField]
+        FruitCtrl[] fruitPrefabs;
+        [ShowInInspector]
+        readonly HashSet<FruitCtrl> activeFruits = new();
+        readonly Dictionary<FruitType, ComponentPool<FruitCtrl>> fruitPools = new();
+
+        [Title("Score")]
+        [SerializeField]
+        int score = 0;
+
+        [Title("Play Area")]
+        [SerializeField]
+        BoxCollider2D playBoundary;
+        [SerializeField]
+        float mergeBounceForce = 2f;
+        
+        public Bounds PlayBounds => playBoundary.bounds;
+
+        public event Action<int> OnScoreChange;
+
+        public event Action OnGameOver;
+        public event Action OnGameOverPPO; // Post process over
 
 
         private void Start() {
@@ -23,21 +52,79 @@ namespace Melon.Game {
 
                 fruitPools.Add(fruit.Type, pool);
             }
+
+            fruitPrefabs = null;
+
+            OnGameOver += _OnGameOver;
         }
 
-        public bool CanUpgrade(FruitType level) {
-            return true;
-            //return level + 1 < fruitPrefabs.Length;
-        }
 
+        public FruitCtrl Get(FruitType type) {
+            var fruit = fruitPools[type].Get();
+            activeFruits.Add(fruit);
+            score += 5;
+            OnScoreChange?.Invoke(score);
+            return fruit;
+        }
+        public void Return(FruitCtrl fruit) {
+            activeFruits.Remove(fruit);
+            fruitPools[fruit.Type].Return(fruit);
+        }
+        public FruitCtrl GetRandomBasedOnScore() => Get(_SelectRandomFruit());
+        public bool CanUpgrade(FruitType level) => (int)level < fruitPools.Count;
         public void MergeAt(Vector2 position, FruitType level) {
-            int nextLevel = Mathf.Min((int)level + 1, fruitPrefabs.Length - 1);
-            var go = Instantiate(fruitPrefabs[nextLevel], position, Quaternion.identity);
-            var rb = go.GetComponent<Rigidbody2D>();
-            if (rb != null) {
-                rb.AddForce(Vector2.up * mergeBounceForce, ForceMode2D.Impulse);
-                rb.AddTorque(Random.Range(-5f, 5f), ForceMode2D.Impulse);
+            FruitType nextLevel = (FruitType)Mathf.Min((int)level + 1, fruitPools.Count);
+            var fruit = Get(nextLevel);
+            fruit.transform.position = position;
+            fruit.PhysicActive();
+            fruit.Bounce(mergeBounceForce);
+            _AddScore(nextLevel);
+        }
+
+        
+        public void GameOver() {
+            if (IsGameOver) return;
+            IsGameOver = true;
+            OnGameOver?.Invoke();
+        }
+
+
+        private async void _OnGameOver() {
+            Debug.Log("[GameManager] GAME OVER");
+
+            foreach (var fruit in activeFruits) {
+                _AddScore(fruit.Type);
+                fruit.PhysicDeactive();
+                fruit.gameObject.SetActive(false);
+                await UniTask.Delay(400);
             }
+
+            OnGameOverPPO?.Invoke();
+        }
+
+        private FruitType _SelectRandomFruit() {
+            int max = 3;
+            if (score > 1200) max = 6; // Max 5
+            else if (score > 800) max = 5; // Max 4
+            else if(score > 300) max = 4;
+            return (FruitType)Random.Range(1, max);
+        }
+
+        private void _AddScore(FruitType type) {
+            switch (type) {
+            case FruitType.Cherry: score += 10; break;
+            case FruitType.Lemon: score += 20; break;
+            case FruitType.Peach: score += 30; break;
+            case FruitType.Apple: score += 50; break;
+            case FruitType.Pear: score += 70; break;
+            case FruitType.Orange: score += 80; break;
+            case FruitType.Mango: score += 100; break;
+            case FruitType.Melon: score += 150; break;
+            case FruitType.Pineapple: score += 200; break;
+            case FruitType.Watermelon: score += 500; break;
+            }
+
+            OnScoreChange?.Invoke(score);
         }
     }
 }
